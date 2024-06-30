afae49fa29bbd6711780e3a09556ccabafb76adc
Lior Pendler <lior.pendler@gmail.com>
2024-06-30 00:07:53 -0800

    Start with supabase

--- api/sync/getfromcode.ts
+++ api/sync/getfromcode.ts
@@ -1,4 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { Database } from '../../database.types';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient<Database>(
+	process.env.SUPABASE_URL,
+	process.env.SUPABASE_KEY,
+);
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
 	const { code } = req.query;
