1f307cd29a9eb5f6d9f589224ac2815babe019b8
Lior Pendler <lior.pendler@gmail.com>
2024-06-30 21:47:44 -0800

    Get is working and start on update

--- api/sync/get.ts
+++ api/sync/get.ts
@@ -7,8 +7,20 @@ const supabase = createClient<Database>(
 	process.env.SUPABASE_KEY,
 );
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
 	const { code } = req.query;
+	const { data, error } = await supabase.from('users').select();
+	if (error) {
+		return res.json({ code: error.code, message: error.message });
+	}
+
+	data.forEach((user) => {
+		if (user.code == code) {
+			return res.json({ ...user });
+		}
+	});
+
+	// If doesnt exist
 	return res.json({
 		message: `Hello ${code}!`,
 	});
---
+++ api/sync/update.ts
@@ -0,0 +1,26 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { Database } from '../../database.types';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient<Database>(
+	process.env.SUPABASE_URL,
+	process.env.SUPABASE_KEY,
+);
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+	const { code } = req.query;
+	const { data, error } = await supabase.from('users').select();
+	if (error) {
+		return res.json({ code: error.code, message: error.message });
+	}
+
+	data.forEach((user) => {
+		if (user.code == code) {
+			return res.json({ ...user });
+		}
+	});
+
+	return res.json({
+		message: `Hello ${code}!`,
+	});
+}
