10b4969c897283ccc03e0a6ad175a835efe90032
Lior Pendler <lior.pendler@gmail.com>
2024-07-01 22:22:23 -0800

    Return code on create

--- api/sync/create.ts
+++ api/sync/create.ts
@@ -55,6 +55,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
 		return res.status(200).json({
 			message: `User with code ${code} has been created successfully`,
+			code: code,
 		});
 	} catch (error) {
 		return res.status(500).json({
