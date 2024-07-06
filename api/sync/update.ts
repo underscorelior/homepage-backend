2999a8fffa3ef18f11879b6fd25e476b7d5fd79f
Lior Pendler <lior.pendler@gmail.com>
2024-07-06 14:37:32 -0700

    Small cleanup

--- api/sync/update.ts
+++ api/sync/update.ts
@@ -11,8 +11,6 @@ const supabase = createClient<Database>(
 const allowCors = (fn) => async (req, res) => {
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	res.setHeader('Access-Control-Allow-Origin', '*');
-	// another common pattern
-	// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 	res.setHeader(
 		'Access-Control-Allow-Methods',
 		'GET,OPTIONS,PATCH,DELETE,POST,PUT',
